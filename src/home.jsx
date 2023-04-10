be546a4d5df13764269f3950a63838f474c41033
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-04-10 14:26:24 +0530

    Added social media

--- src/home.jsx
+++ src/home.jsx
@@ -12,7 +12,8 @@ function Home() {
           <h3>Student and Developer</h3>
         </div>
         <div className='icons'>
-          <img src='https://static-00.iconduck.com/assets.00/linkedin-icon-512x512-vkm0drb1.png' alt='logo'></img>
+        <a href='https://www.linkedin.com/in/harish-sekar-369506201/'><img src='https://static-00.iconduck.com/assets.00/linkedin-icon-512x512-vkm0drb1.png' alt='logo'></img></a>
+          <a href='https://github.com/harishsekar1402'><img className='git' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt='logo'></img></a>
           <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png" alt="tweet" />
         </div>
     </div>
